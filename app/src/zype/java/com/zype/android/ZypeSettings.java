abd01ea80221f804320118d1bae459c47299d603
Eugeny Cherkasov <ech89899@gmail.com>
2018-02-23 20:25:25 +0300

    Fixed display long playlist title

--- app/src/zype/java/com/zype/android/ZypeSettings.java
+++ app/src/zype/java/com/zype/android/ZypeSettings.java
@@ -36,8 +36,8 @@ public class ZypeSettings {
 
     // Features
     public static final boolean BACKGROUND_PLAYBACK_ENABLED = false;
-    public static final boolean DOWNLOADS_ENABLED = false;
-    public static final boolean DOWNLOADS_ENABLED_FOR_GUESTS = false;
+    public static final boolean DOWNLOADS_ENABLED = true;
+    public static final boolean DOWNLOADS_ENABLED_FOR_GUESTS = true;
     public static final boolean SHARE_VIDEO_ENABLED = false;
 
     public static final String THEME = ZypeConfiguration.THEME_LIGHT;
