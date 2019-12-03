bf0ac2663ec6540453d0bc52de6044bbc14fa953
Evgeny Cherkasov <ech89899@gmail.com>
2019-12-03 22:20:37 +0300

    Add period and trial text to plans screen if applicable  (#38)
    * Add period next to the price on the Plans page for subscription plans - apps-673
    * Add 'After Free Trial' text next to price if there is a trial on the product in google play store - apps-673

--- app/src/main/java/com/zype/android/ui/Subscription/SubscriptionActivity.java
+++ app/src/main/java/com/zype/android/ui/Subscription/SubscriptionActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -289,7 +290,18 @@ public class SubscriptionActivity extends BaseActivity implements BillingManager
             holder.item = items.get(position);
             if (holder.item.getMarketplace() != null) {
                 holder.textTitle.setText(holder.item.getMarketplace().getTitle());
-                holder.textPrice.setText(String.valueOf(holder.item.getMarketplace().getPrice()));
+                String periodText = getPeriodText(holder.item.getMarketplace().getSubscriptionPeriod());
+                String price;
+                if (TextUtils.isEmpty(periodText)) {
+                    price = String.valueOf(holder.item.getMarketplace().getPrice());
+                }
+                else {
+                    price = String.format("%1$s/%2$s", holder.item.getMarketplace().getPrice(), periodText);
+                }
+                if (!TextUtils.isEmpty(holder.item.getMarketplace().getFreeTrialPeriod())) {
+                    price += " (After Free Trial)";
+                }
+                holder.textPrice.setText(price);
                 holder.textDescription.setText(holder.item.getMarketplace().getDescription());
                 holder.buttonContinue.setText(String.format(getString(R.string.subscription_item_button_continue), holder.item.getMarketplace().getTitle()));
                 holder.buttonContinue.setOnClickListener(new View.OnClickListener() {
@@ -322,6 +334,17 @@ public class SubscriptionActivity extends BaseActivity implements BillingManager
                 textPrice = view.findViewById(R.id.textPrice);
                 textDescription = view.findViewById(R.id.textDescription);
                 buttonContinue = view.findViewById(R.id.buttonContinue);
+            }
+        }
+
+        private String getPeriodText(String subscriptionPeriod) {
+            switch (subscriptionPeriod) {
+                case "P1M":
+                    return "month";
+                case "P1Y":
+                    return "year";
+                default:
+                    return "";
             }
         }
     }
