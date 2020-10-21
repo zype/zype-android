4f940ad27fda1e754fb95f6443e98ccc6d0fa7c8
anurag <anuragdce@gmail.com>
2020-10-21 19:36:43 +0530

    removed keys.

--- app/src/zype/java/com/zype/android/ZypeSettings.java
+++ app/src/zype/java/com/zype/android/ZypeSettings.java
@@ -6,20 +6,20 @@ package com.zype.android;
 
 public class ZypeSettings {
     // Zype app key
-    public static final String APP_KEY = "zCNqYpQAzxTg8aZOPeiQCovML09M7pQsQ6Mb_aRtW7riYdv1tzCcKDW7BkPMvf17";
+    public static final String APP_KEY = "<APP_KEY>";
     // OAuth credentials
-    public static final String CLIENT_ID = "03fe1c1c964ceeff2312f25dcbc7ff3b70d10bb776d78b272b943a48fba2afd0";
+    public static final String CLIENT_ID = "<CLIENT_ID>";
     // Playlist
-    public static final String ROOT_PLAYLIST_ID = "5c59b7be45793d112d003a7b";
+    public static final String ROOT_PLAYLIST_ID = "<ROOT_PLAYLIST_ID>";
 
     // Google Analytics Tracking Id
     public static final String GA_TRACKING_ID = "";
 
     // Social
-    public static final String FACEBOOK_ID = "zypevideo";
-    public static final String INSTAGRAM_ID = "zype";
-    public static final String TWITTER_ID = "zype";
-    public static final String WEB_URL = "https://www.zype.com/";
+    public static final String FACEBOOK_ID = "";
+    public static final String INSTAGRAM_ID = "";
+    public static final String TWITTER_ID = "";
+    public static final String WEB_URL = "";
 
     // Monetization
     public static final boolean NATIVE_SUBSCRIPTION_ENABLED = false;
