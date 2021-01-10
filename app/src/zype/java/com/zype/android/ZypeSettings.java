4edb52375641b5fee51e754a0c43081d7d542106
Evgeny Cherkasov <ech89899@gmail.com>
2021-01-10 23:20:25 +0300

    Enable Marketplace Connect when only NATIVE_TVOD is true, without needing to set UNIVERSAL_TVOD

--- app/src/zype/java/com/zype/android/ZypeSettings.java
+++ app/src/zype/java/com/zype/android/ZypeSettings.java
@@ -28,7 +28,7 @@ public class ZypeSettings {
     public static final String[] PLAN_IDS = new String[] { "5b366329849e2d140c00002f", "5b366368849e2d141400002b" };
     public static final boolean SUBSCRIBE_TO_WATCH_AD_FREE_ENABLED = false;
     public static final boolean UNIVERSAL_SUBSCRIPTION_ENABLED = false;
-    public static final boolean UNIVERSAL_TVOD = true;
+    public static final boolean UNIVERSAL_TVOD = false;
     public static final boolean PLAYER_PAYWALL_ENABLED = true;
 
     // Features
